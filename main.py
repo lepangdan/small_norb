a34f46b24dc6bf42f9353ee118dc7489c74f43f3
pangdan <lepangdan@outlook.com>
2019-08-01 17:55:13 +0800

    ddd

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ if __name__ == '__main__':
 
     # Initialize the dataset from the folder in which
     # dataset archives have been uncompressed
-    # dataset = SmallNORBDataset(dataset_root='../datasets/smallnorb/')
+    dataset = SmallNORBDataset(dataset_root='../datasets/smallnorb/')
     #
     # # Dump all images to disk
-    # dataset.export_to_jpg(export_dir='smallnorb_export')
+    dataset.export_to_jpg(export_dir='smallnorb_export')
 
     # Explore random examples of the training set
     # to show how data look like
@@ -28,20 +28,20 @@ if __name__ == '__main__':
     # files = [f for f in glob.glob(os.join(parent_data_path) + "**/*.txt", recursive=True)]
 
 
-    # # load the image
-    # image = Image.open('smallnorb_export/train/012149_car_04_rt.jpg')
-    # # summarize some details about the image
-    # image.load()
-    # data = np.asarray(image, dtype="int32")
-    # print(image.format)
-    # print(image.mode)
-    # print(image.size)
-    # print(type(data))
-    # print('max data:', np.amax(data))
-    # print('min data:', np.amin(data))
+    # load the image
+    image = Image.open('smallnorb_export/train/012149_car_04_rt.jpg')
+    # summarize some details about the image
+    image.load()
+    data = np.asarray(image, dtype="int32")
+    print(image.format)
+    print(image.mode)
+    print(image.size)
+    print(type(data))
+    print('max data:', np.amax(data))
+    print('min data:', np.amin(data))
 
-    (x_train, y_train) = load_data('train')
-    print(x_train,y_train)
+    # (x_train, y_train) = load_data('train')
+    # print(x_train,y_train)
 
 
     # show the image
