a94083cfe1e269f86c3cafdb0ebcdc9f5ca56628
pangdan <lepangdan@outlook.com>
2019-08-01 20:11:23 +0800

    ggg

--- utils.py
+++ utils.py
@@ -367,7 +367,7 @@ def get_training_data():
 
     for idx in range(num_images):
         temp = fid_images.read(96 * 96)
-        print('temp:',temp)
+        print('temp:',np.fromstring(temp, 'uint8'))
         images[idx, :, :] = np.fromstring(temp, 'uint8').reshape(96, 96).T
 
     for i in range(5):
